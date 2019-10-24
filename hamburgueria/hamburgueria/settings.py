ba4ba0de721385a7c3bc258898eda4723980699b
rafael <rafael.cmota@hotmail.com>
2019-10-24 10:15:16 -0300

    commit 241020191015

--- hamburgueria/hamburgueria/settings.py
+++ hamburgueria/hamburgueria/settings.py
@@ -93,14 +93,19 @@ WSGI_APPLICATION = 'hamburgueria.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hamburgueria',
-        'USER': 'postgres',
-        'PASSWORD': 'RaX3)9fAx#0(',
-        'HOTS': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'hamburgueria',
+    #    'USER': 'postgres',
+    #    'PASSWORD': 'RaX3)9fAx#0(',
+    #    'HOTS': 'localhost',
+    #    'PORT': 5432,
+    #}
 }
 
 
