12ec0a5e96d5927c1ad6b40bc30afb0fe80bcc76
Rafael <rafael.cmota@hotmail.com>
2022-10-19 15:03:56 -0300

    Adicionando BACKEND JAVA SPRING BOOT

---
+++ BACKEND/backendHamburgueria/src/main/java/br/com/api/v1/backendHamburgueria/BackendHamburgueriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.v1.backendHamburgueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHamburgueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHamburgueriaApplication.class, args);
+	}
+
+}
